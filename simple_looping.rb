fc6b71c24d3e3967b0a3e5f7c653ac0b5d69a149
Kelly Sheddy <jungaka07@gmail.com>
2018-09-30 22:47:53 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,12 +1,14 @@
 # REMEMBER: print your output to the terminal using 'puts'
 
 def loop_iterator(number_of_times)
+  counter = 0
   loop do
-    puts "Welcome to Flatiron School's Web Development Course!"
+    counter += 1
+    puts phrase
+    if counter >= 7
+        break
+    end
   end
-end
-
-
 
 def times_iterator(number_of_times)
   7.times do
